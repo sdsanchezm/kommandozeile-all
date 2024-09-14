cb195b7ac1b468af551778d1c4b66dc6ab810918
sdsanchezm <sdsanchezm@gmail.com>
2024-09-14 12:04:25 -0500

    adding: host windows

---
+++ windows_ssfs/host.ps1
@@ -0,0 +1,15 @@
+
+[string] $okHosts = "127.0.0.1       localhost
+::1             localhost
+127.0.0.1  data.microsoft.com
+127.0.0.1  activity.windows.com
+127.0.0.1  edge.microsoft.com
+127.0.0.1  msedge.net
+127.0.0.1  scorecardresearch.com
+127.0.0.1  msftconnecttest.com
+127.0.0.1  bingapis.com
+127.0.0.1  azureedge.net
+127.0.0.1  assets.msn.com
+127.0.0.1  data.msn.com";
+
+[string] $okPath = "C:\Windows\System32\drivers\etc";
