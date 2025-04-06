7bf710621b5cf779a3162533b8ceafdafe737815
sdsanchezm <sdsanchezm@gmail.com>
2025-04-05 20:13:09 -0500

    updating ssfs script

--- windows_ssfs/ssfs.ps1
+++ windows_ssfs/ssfs.ps1
@@ -3,27 +3,32 @@
 # Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 # Set-ExecutionPolicy Restricted -Scope CurrentUser
 
-$machineName = "redenhaput"
+$mainFolder = "ssfs"
 
 $folders = @(
-    "apps",
-    "apps_portable",
-    "bilder",
-    "books_all",
-    "cygwin64",
-    "cygwin64Packages",
-    "musik", 
-    "progs",
-    "temp",
-    "shared_ss_${machineName}",
-    "VMsAll",
-    "ssfs",
-    "ssfs\Modules"
+    "${mainFolder}\apps_downs",
+    "${mainFolder}\apps_portable",
+    "${mainFolder}\apps_installed",
+    "${mainFolder}\bilder",
+    "${mainFolder}\books_all",
+    "${mainFolder}\cygwin64",
+    "${mainFolder}\cygwin64Packages",
+    "${mainFolder}\DriversForThisComputer",
+    "${mainFolder}\musik", 
+    "${mainFolder}\progs",
+    "${mainFolder}\temp",
+    "${mainFolder}\shared_folder",
+    "${mainFolder}\VMsAll",
+    "${mainFolder}\xampp",
+    "${mainFolder}\MegaSyncAll",
+    "${mainFolder}\GoogleDriveAll",
+    "${mainFolder}\DropboxSyncAll"
     )
 
-foreach ($folder in $folders)
-{
+foreach ($folder in $folders) {
+    # New-Item -Path "C:\${mainFolderName}" -ItemType Directory
     $folderPath = Join-Path -Path "C:\" -ChildPath $folder
-    New-Item -Path $folderPath -ItemType Directory
+    # New-Item -Path $folderPath -ItemType Directory
+    if (-Not (Test-Path -Path $folderPath)) { New-Item -Path $folderPath -ItemType Directory }
     Write-Host "Created folder: $folderPath"
 }
